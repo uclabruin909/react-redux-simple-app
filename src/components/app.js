9c67274c017132ff3aaa916e0cbf0e570da2c68d
Kevin Kim <kevintaekim80@gmail.com>
2018-07-23 14:06:11 -0700

    create initial PlayerList component and render

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import PlayerList from './player-list';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React Redux Example</div>
+      <div>
+        <PlayerList />
+      </div>
     );
   }
 }
---
+++ src/components/player-list.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PlayerList extends Component {
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        <li>Test Link1</li>
+        <li>Test Link2</li>
+        <li>Test Link3</li>
+        <li>Test Link4</li>
+      </ul>
+    );
+  }
+}
+
+export default PlayerList;
