a1a60650ab5a535483e45309e8b80ef26e34aa54
Kevin Kim <kevintaekim80@gmail.com>
2018-07-27 14:53:03 -0700

    import activePlayer reducer and init activePlayer state prop in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 //app specific reducers
 import PlayersReducers from './reducer_players';
+import ActivePlayerReducers from './reducer_activePlayer';
 
 //map the state to the appropriate reducers
 const rootReducer = combineReducers({
   players: PlayersReducers,
+  activePlayer: ActivePlayerReducers,
 });
 
 export default rootReducer;
