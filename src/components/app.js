a0ccb13710c5536e6bdac18e8a12c5ba24e004b7
Kevin Kim <kevintaekim80@gmail.com>
2018-07-27 14:50:13 -0700

    create initial playerDetail component class

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import PlayerList from './player-list';
+import PlayerDetail from './player-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <PlayerList />
+        <PlayerDetail />
       </div>
     );
   }
---
+++ src/components/player-detail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PlayerDetail extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Player Detail:</h3>
+			</div>
+		);
+	}
+}
+
+export default PlayerDetail;
